73c4b45603402d7b2979831c5926771707718ab7
Keirafranco <125355714+Keirafranco@users.noreply.github.com>
2023-03-02 09:47:42 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+basic.forever(function () {
+	
+})
